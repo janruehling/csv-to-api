46bdbb8b53d2ccc8f504b92f209d579bd848b121
Josh Begley <josh@joshbegley.com>
2014-11-09 20:56:26 -0500

    added cross-origin headers

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 # Include Instant API's function library.
 require_once('class.csv-to-api.php');
